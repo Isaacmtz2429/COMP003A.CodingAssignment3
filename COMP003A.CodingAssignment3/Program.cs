385ccb33dc0babe6e8e1cbe3191243338fecb685
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-01-29 21:57:08 -0800

    Add project files.

---
+++ COMP003A.CodingAssignment3/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+// Author: Isaac Martinez
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: Budget management application demonstrating control flow.
+*/
+namespace COMP003A.CodingAssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Budget Management Tool!");
+        }
+    }
+}
